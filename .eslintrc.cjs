9347b859e1aabe5b84284ec2bf06c1525c6a8d75
Breg,Coen C.J <c.breg@student.fontys.nl>
2022-08-09 16:06:41 +0200

    feat: prettier ran

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,8 +5,16 @@ module.exports = {
   root: true,
   extends: [
     'plugin:vue/vue3-essential',
+    'prettier',
     'eslint:recommended',
     '@vue/eslint-config-typescript/recommended',
     '@vue/eslint-config-prettier'
-  ]
+  ],
+  rules: {
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'vue/multi-word-component-names': 'off'
+  }
 };
