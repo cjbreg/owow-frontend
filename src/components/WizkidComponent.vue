ed4672cd49abd6e991a79885405e61326d2b5503
Breg,Coen C.J <c.breg@student.fontys.nl>
2022-08-09 13:23:24 +0200

    feat: created wizkid component

---
+++ src/components/WizkidComponent.vue
@@ -0,0 +1,36 @@
+<script lang="ts">
+import Icon from './icons/Icon.vue';
+export default {
+  props: {
+    wizkid: {
+      type: Object,
+      required: true
+    }
+  },
+  components: { Icon }
+};
+</script>
+
+<template>
+  <div class="flex container justify-between text-white border rounded-2xl px-2 py-3 mb-4">
+    <div class="flex">
+      <div class="flex items-center">
+        <div
+          class="text-gray-300 border border-gray-500 flex justify-center items-center p-6 rounded-2xl mr-3">
+          <Icon name="image" />
+        </div>
+        <div>
+          <h1 class="text-lg font-semibold">{{ wizkid.name }}</h1>
+          <h1 class="text-sm">{{ wizkid.email }}</h1>
+          <h1 class="text-sm capitalize">{{ wizkid.role }}</h1>
+        </div>
+      </div>
+    </div>
+
+    <div class="text-white flex justify-center items-center p-2 rounded-2xl mr-2">
+      <Icon name="chevron-right" :width="32" :height="32" />
+    </div>
+  </div>
+</template>
+
+<style></style>
