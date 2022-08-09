88906821a0cf4efb7dd67970f95311da4f60ef49
Breg,Coen C.J <c.breg@student.fontys.nl>
2022-08-09 14:42:25 +0200

    feat: added basic laylout for testing

--- src/views/Wizkid/Profile.vue
+++ src/views/Wizkid/Profile.vue
@@ -1,24 +1,36 @@
 <script lang="ts">
-import type { Wizkid } from '@/models/Wizkid';
-import { Role } from '@/models/Wizkid';
 import { storeToRefs } from 'pinia';
 import { useWizkidStore } from '@/stores/wizkid';
-import WizkidComponent from '../../components/WizkidComponent.vue';
+import { useRoute } from 'vue-router';
+import { Role } from '@/models/Wizkid';
+import type { Wizkid } from '@/models/Wizkid';
 const { wizkid } = storeToRefs(useWizkidStore());
+const { updateWizkid, removeWizkid } = useWizkidStore();
+
+const route = useRoute();
+
 export default {
   data() {
     return {
       wizkid
     };
   },
-  created() {},
-  components: { WizkidComponent },
   methods: {
-    getCurrentWizkid(id: string) {
-      console.log(id);
+    getCurrentWizkid() {
+      console.log(route.params.id);
     },
-    handleAddNew() {
-      this.$router.push('/wizkid/add');
+    handleUpdateWizkid() {
+      const updatedWizkid: Wizkid = {
+        id: 'de45966c-ffb4-4452-baef-5fea44c7e8a6',
+        name: 'Coenio ',
+        email: 'coenbreg@gmail.com',
+        role: Role.ITERN,
+        picture: 'asg'
+      };
+      updateWizkid(updatedWizkid);
+    },
+    handleRemoveWizkid(id: string) {
+      removeWizkid(id);
     }
   }
 };
@@ -29,11 +41,25 @@ export default {
     <div class="mb-4 text-white">
       <h1 class="font-semibold text-lg">Wizkid: {{ wizkid.name }} ( {{ $route.params.id }} )</h1>
     </div>
-    <button
-      class="disabled:bg-gray-300 w-32 py-1 font-medium bg-green-700 text-white rounded-md hover:bg-green-600 transition duration-150"
-      @click="handleAddNew(newKid)">
-      Add new
-    </button>
+    <div class="flex flex-col">
+      <p>{{ wizkid.name }}</p>
+      <p>{{ wizkid.email }}</p>
+      <p>{{ wizkid.role }}</p>
+      <p>{{ wizkid.picture }}</p>
+    </div>
+    <div class="flex my-3">
+      <button
+        @click="handleUpdateWizkid"
+        class="disabled:bg-gray-300 w-32 py-1 font-medium bg-green-700 text-white rounded-md hover:bg-green-600 transition duration-150 mr-4">
+        Update
+      </button>
+
+      <button
+        @click="handleRemoveWizkid($route.params.id)"
+        class="disabled:bg-gray-300 w-32 py-1 font-medium bg-red-700 text-white rounded-md hover:bg-red-600 transition duration-150">
+        Delete
+      </button>
+    </div>
   </div>
 </template>
 
