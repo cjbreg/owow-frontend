e8a65e6226a013d91f193f9590d987c30983ce1f
Breg,Coen C.J <c.breg@student.fontys.nl>
2022-08-09 10:21:49 +0200

    chore: test pinia flow

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,12 +1,28 @@
 <script lang="ts">
+import { v4 as uuidv4 } from 'uuid';
+import type { Wizkid } from '@/models/wizkid';
 import { storeToRefs } from 'pinia';
 import { useWizkidStore } from '../stores/wizkid';
 const { wizkids, loading } = storeToRefs(useWizkidStore());
+const { addWizkid } = useWizkidStore();
+
+const HandleAddWizkid = () => {
+  const testWizkid: Wizkid = {
+    id: uuidv4(),
+    name: 'Coen',
+    role: 'admin',
+    email: 'email',
+    picture: 'picture'
+  };
+  addWizkid(testWizkid);
+};
+
 export default {
   data() {
     return {
       loading,
-      wizkids
+      wizkids,
+      HandleAddWizkid
     };
   }
 };
@@ -16,6 +32,7 @@ export default {
   <div class="about">
     <h1>This is an about page</h1>
     <p v-if="!loading">{{ loading }} {{ wizkids.length }}</p>
+    <button @click="HandleAddWizkid">test</button>
   </div>
 </template>
 
