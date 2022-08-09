4a023cc3b6e6b0ae9ab73fd3bd5b1cc4b3a9d546
Breg,Coen C.J <c.breg@student.fontys.nl>
2022-08-09 17:01:31 +0200

    feat: added filter

---
+++ src/components/RoleFilterComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div v-if="option === value" class="mr-2 text-green-500 capitalize" @click="onClick">
+    <slot />
+  </div>
+  <div v-else class="mr-2 capitalize" @click="onClick"><slot /></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  props: ['option', 'value'],
+  methods: {
+    onClick() {
+      console.log(this.option);
+      this.$emit('input', this.option);
+    }
+  }
+});
+</script>
--- src/views/Wizkid/index.vue
+++ src/views/Wizkid/index.vue
@@ -4,16 +4,24 @@ import { useWizkidStore } from '@/stores/wizkid';
 import WizkidComponent from '../../components/WizkidComponent.vue';
 const { wizkids } = storeToRefs(useWizkidStore());
 import { defineComponent } from 'vue';
+import RoleFilterComponent from '../../components/RoleFilterComponent.vue';
+import { Role } from '@/models/Wizkid';
 export default defineComponent({
   data() {
     return {
-      wizkids
+      wizkids,
+      selectedRole: null,
+      roles: [Role.BOSS, Role.DESIGNER, Role.DEVELOPER, Role.ITERN]
     };
   },
-  components: { WizkidComponent },
+  components: { WizkidComponent, RoleFilterComponent },
   methods: {
     handleAddNew() {
       this.$router.push('/wizkid/add');
+    },
+    updateFilter(x: any) {
+      if (this.selectedRole === x) return (this.selectedRole = null);
+      this.selectedRole = x;
     }
   }
 });
@@ -24,8 +32,24 @@ export default defineComponent({
     <div class="mb-4 text-white">
       <h1 class="font-semibold text-lg">Wizkids ( {{ wizkids.length }} )</h1>
     </div>
-    <div>
-      <WizkidComponent v-for="(wizkid, index) in wizkids" :key="index" :wizkid="wizkid" />
+    <div class="flex flex-col mb-3">
+      <span class="text-white">Filter by:</span>
+      <div class="flex flex-row">
+        <RoleFilterComponent
+          v-for="(role, index) in roles"
+          :key="index"
+          :option="role"
+          :value="selectedRole"
+          @input="(i) => updateFilter(i)">
+          {{ role }}</RoleFilterComponent
+        >
+      </div>
+    </div>
+    <div v-for="(wizkid, index) in wizkids">
+      <WizkidComponent
+        :key="index"
+        :wizkid="wizkid"
+        v-if="wizkid.role === selectedRole || selectedRole === null" />
     </div>
     <button
       class="disabled:bg-gray-300 w-32 py-1 font-medium bg-green-700 text-white rounded-md hover:bg-green-600 transition duration-150"
