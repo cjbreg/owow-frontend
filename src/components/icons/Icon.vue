c9620a93b8135c52b2f4c6bd00a267e513a0586d
Breg,Coen C.J <c.breg@student.fontys.nl>
2022-08-09 13:22:59 +0200

    feat: added feather icons

---
+++ src/components/icons/Icon.vue
@@ -0,0 +1,43 @@
+<script lang="ts">
+export default {
+  props: {
+    name: {
+      type: String,
+      default: 'feather'
+    },
+    width: {
+      type: [Number, String],
+      default: 24
+    },
+    height: {
+      type: [Number, String],
+      default: 24
+    },
+    strokeWidth: {
+      type: [Number, String],
+      default: 1.5
+    }
+  }
+};
+</script>
+
+<template>
+  <svg
+    class="feather-icon"
+    :width="width"
+    :height="height"
+    :stroke-width="strokeWidth"
+    xmlns="http://www.w3.org/2000/svg">
+    <use v-bind="{ 'xlink:href': '/assets/feather-sprite.svg#' + name }" />
+  </svg>
+</template>
+
+<style scoped>
+.feather-icon {
+  stroke: currentColor;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  fill: none;
+  display: inline-block;
+}
+</style>
